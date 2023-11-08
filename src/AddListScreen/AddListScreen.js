2e1fc42b026e5972cafc4684da499b0ff6147e89
JoaoVSantana <joao10victorsantana@gmail.com>
2023-11-07 21:56:25 -0300

    Campo de texto melhorado

--- src/AddListScreen/AddListScreen.js
+++ src/AddListScreen/AddListScreen.js
@@ -1,14 +1,41 @@
 import { useEffect, useState } from "react";
-import { Button, Text, View } from "react-native";
+import { Button, Text, View, TextInput, StyleSheet } from "react-native";
 
-const AddListScreen = ({ navigation }) => {
+export default AddListScreen = ({ navigation }) => {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <View style={styles.container}>
+
             <Text>Adicionar Lista</Text>
-            
-            <Button title="Voltar" onPress={() => navigation.navigate("Home")}/>
+
+            <TextInput style={styles.input} placeholder="Digite o nome da lista"></TextInput>
+
+            <Button title="Adicionar Lista" onPress={() => navigation.navigate("Home")} />
+
+
         </View>
     );
 }
 
-export default AddListScreen;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        gap: 15,
+        padding: 15,
+        backgroundColor: '#eee',
+        alignItems: 'center',
+        width: "100%",
+    },
+
+    input: {
+
+        width: "90%",
+        padding: 15,
+        fontSize: 18,
+        borderWidth: 3,
+        backgroundColor:'#CECBCA',
+        borderColor: "#181014",
+        borderStyle: "solid",
+        borderRadius: 5,
+        placeholderTextColor:'#000000',
+    }
+})
--- src/HomeScreen/HomeScreen.js
+++ src/HomeScreen/HomeScreen.js
@@ -1,14 +1,49 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useEffect, useState } from "react";
-import { Button, Text, View } from "react-native";
+import { Pressable, Text, View, StyleSheet } from "react-native";
 
-const HomeScreen = ({ navigation }) => {
+export default HomeScreen = ({ navigation }) => {
+
+    const [listas, setListas] = useState(new Array());
+    
+
+    const getUserName = async () => {
+        const userName = await AsyncStorage.getItem(metadata.USER.USERNAME);
+        if (userName) {
+            setName(userName);
+        }
+    }
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <View style={styles.container}>
             <Text>Home Screen</Text>
-            
-            <Button title="Add List" onPress={() => navigation.navigate("AddListScreen")}/>
+            <Pressable style={styles.botao} onPress={() => navigation.navigate("AddListScreen")} >
+                <Text style={styles.texto}>Add Lista</Text>
+            </Pressable>
         </View>
     );
 }
 
-export default HomeScreen;
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        gap: 15,
+        padding: 15,
+        backgroundColor: '#eee',
+        alignItems: 'center',
+        width: "100%",
+    },
+   
+    botao:{
+        backgroundColor: '#13abde',
+        width:150,
+        height:50,
+    },
+
+    texto:{
+        flex: 1,
+        justifyContent: 'center',
+        alignSelf: 'center',
+        
+    }
+})
